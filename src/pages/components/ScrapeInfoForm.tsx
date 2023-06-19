f0aa95e5f29d0533c3fd037e8ff4b1ac969fccdf
Divesh <diveshpatel39@gmail.com>
2023-06-19 11:53:46 -0700

    Edits to gitignore

--- src/pages/components/ScrapeInfoForm.tsx
+++ src/pages/components/ScrapeInfoForm.tsx
@@ -1,9 +1,18 @@
 
 const ScrapeInfoForm = ()=>{
+
+
+  const submitData=(e:any)=>{
+    console.log(e);
+    
+  }
+
+
+
     return(
 
 <div className="w-full max-w-xs">
-  <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+  <form onClick={(e)=>{submitData(e)}} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
     <div className="mb-4">
       <label className="block text-gray-700 text-2xl font-bold mb-2" htmlFor="username">
         Username
