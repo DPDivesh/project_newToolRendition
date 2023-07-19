95880033dbc01f9e82a50849634984e1d1d110c5
Divesh <diveshpatel39@gmail.com>
2023-07-18 17:56:46 -0700

    making fixes to allow building

--- src/pages/components/Squares.tsx
+++ src/pages/components/Squares.tsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SquaresContent from './SquaresContent.tsx'
 
 
 const Squares =(props:any)=>{
 
+
+  const arrayOfSquares = props.backendData && Array.isArray(props.backendData)
+  ? props.backendData.map((data: any, index: number) => (
+      <SquaresContent data={data} key={index} />
+    ))
+  : null;
+
+  
   return(
     <div className='bg-indigo-400'> 
    <div className="justify-items-center grid md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 mx-50 ">
         {
-          
-        Array.from(props.backendData).map((data:any, index) =>{        
-          return(
-            <SquaresContent data={data} key={index} />
-        )},
-
-        )
+          arrayOfSquares
+       
         }
         
      
