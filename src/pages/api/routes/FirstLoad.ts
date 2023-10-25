50e4191e7c239f259dfaf4b621efea07b1e64eb4
Divesh <diveshpatel39@gmail.com>
2023-10-25 13:41:47 -0700

    Cleaned up data fetching and db processing

--- src/pages/api/routes/FirstLoad.ts
+++ src/pages/api/routes/FirstLoad.ts
@@ -1,16 +1,19 @@
-import { NextApiRequest, NextApiResponse } from "next"
-import prisma from "../client"
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../client";
 
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  //make a conditional that loads from xlsx file first and if that file doenst exist make a db request.
+  // let initialLoad = await prisma.posts.findMany({where:{userEmail:req.body.email}})
+  let initialLoad = await prisma.posts.findMany({
+    where: { userEmail: req.body.email, NOT: { storeName: "undefined" } },
+  });
 
-export default async function handler(req:NextApiRequest,res:NextApiResponse){
-//make a conditional that loads from xlsx file first and if that file doenst exist make a db request.
-// let initialLoad = await prisma.posts.findMany({where:{userEmail:req.body.email}})
-let initialLoad = await prisma.posts.findMany({where:{userEmail:req.body.email, NOT: {storeName:"undefined"}}})
-
-// let useremail = req.body
-res.status(200).json(initialLoad)
+  // let useremail = req.body
+  res.status(200).json(initialLoad);
 }
-
 
 // {
 //     "email": "diveshcodes@gmail.com"
--- src/pages/components/SquaresContent.tsx
+++ src/pages/components/SquaresContent.tsx
@@ -1,98 +1,147 @@
-import {useState,useEffect} from "react";
+import { useState, useEffect, useRef } from "react";
 import InfoForm from "./InfoForm.tsx";
 import axios from "axios";
-import { useSession } from "next-auth/react"
+import { useSession } from "next-auth/react";
+import { useOnClickOutside } from "usehooks-ts";
 
-const SquaresContent =(props:any)=>{
-  
+const SquaresContent = (props: any) => {
+  const ref = useRef(null);
 
-  const { data: session, status } = useSession()
-  const [name,setName] = useState(props.data?.storeName)
-  const [CashTotal,setCashTotal] = useState(props.data?.cashBalance)
-  const [balanceAsOf,setBalanceAsOf] = useState(props.data?.balanceAsOf)
-  const [lastCommunication,setLastCommunication] = useState(props.data?.lastCommunication)
-  const [TerminalID,setTerminalID] = useState(props.data?.TerminalId)
-  const [minCash, setMinCash] = useState(props.data?.minReload)
-  const [update,setUpdate] = useState(false)
-  const refreshData=()=>{
-    
+  const { data: session, status } = useSession();
+  const [name, setName] = useState(props.data?.storeName);
+  const [CashTotal, setCashTotal] = useState(props.data?.cashBalance);
+  const [balanceAsOf, setBalanceAsOf] = useState(props.data?.balanceAsOf);
+  const [lastCommunication, setLastCommunication] = useState(
+    props.data?.lastCommunication
+  );
+  const [TerminalID, setTerminalID] = useState(props.data?.TerminalId);
+  const [minCash, setMinCash] = useState(props.data?.minReload);
+  const [update, setUpdate] = useState(false);
+  // const refreshData = () => {
+  //   setCashTotal(props.data.cashBalance);
+  // };
+  const handleClickOutside = () => {
+    // Your custom logic here
+    setUpdate(false);
+    console.log("clicked outside");
+  };
+  const handleClickInside = () => {
+    // Your custom logic here
+    setUpdate(true);
 
-    setCashTotal(props.data.cashBalance);
-  }
+    console.log("clicked inside");
+  };
 
-  const parentForm=(data:boolean)=>{
-    setUpdate(data)
-  }
+  useOnClickOutside(ref, handleClickOutside);
 
+  const parentForm = (data: boolean) => {
+    setUpdate(data);
+  };
 
-const reloadUpdateHandler = (e:any)=>{
-  e.preventDefault()
-//need to figure out how to get specific terminal id in here
+  const reloadUpdateHandler = (e: any) => {
+    e.preventDefault();
+    //need to figure out how to get specific terminal id in here
 
-axios.post("/api/routes/updateReloadAmount",{
-  email:session?.user?.email,
-  TerminalID: TerminalID,
-  minCash:Intl.NumberFormat('en-US').format( e.target[0].value)
-})
+    axios.post("/api/routes/updateReloadAmount", {
+      email: session?.user?.email,
+      TerminalID: TerminalID,
+      minCash: Intl.NumberFormat("en-US").format(e.target[0].value),
+    });
 
-setMinCash(Intl.NumberFormat('en-US').format( e.target[0].value))
-setUpdate(false)
-}
+    setMinCash(Intl.NumberFormat("en-US").format(e.target[0].value));
+    setUpdate(false);
+  };
 
-const minReloadHandler =((e:any)=>{
-  e.preventDefault()
-  setMinCash(e.target.value)
-})
+  const minReloadHandler = (e: any) => {
+    e.preventDefault();
+    setMinCash(e.target.value);
+  };
 
-const updateHandler = () =>{
-  update ?  setUpdate(false) : setUpdate(true)
+  // const updateHandler = () => {
+  //   update ? setUpdate(false) : setUpdate(true);
+  // };
 
-  
-}
+  const withUpdateForm = (
+    <div
+      className="squares bg-white text-black my-4 max-w-xs rounded-lg border-gray-500 border-1 shadow-2xl m-0"
+      ref={ref}
+    >
+      <div className="square-content flex flex-col mx-5 my-2">
+        <h2 className="text-black">
+          Name: <u>{name}</u>
+        </h2>
+        <h3>
+          Current Balance:{" "}
+          <span
+            className={CashTotal <= minCash ? "text-red-500" : "text-green-500"}
+          >
+            {CashTotal}
+          </span>
+        </h3>
+        <h3>
+          Balance Update: <span style={{ color: "green" }}>{balanceAsOf} </span>
+        </h3>
+        <h3>
+          <span style={{ color: "grey" }}>
+            Last Update: {lastCommunication}
+          </span>
+        </h3>
+        <h3>
+          <span style={{ color: "grey" }}>ID: {TerminalID}</span>
+        </h3>
+        <h3>
+          <span style={{ color: "grey" }}>Min Reload: {minCash}</span>
+        </h3>
+        <form id="minimal-reload" onSubmit={reloadUpdateHandler}>
+          <InfoForm
+            reload={minReloadHandler}
+            parentFormFunction={parentForm}
+            minCash={minCash}
+          />
+        </form>
+      </div>
+    </div>
+  );
 
-const withUpdateForm=<div className="squares bg-white text-black my-4 max-w-xs rounded-lg border-gray-500 border-1 shadow-2xl m-0"   >
-            <div className="square-content flex flex-col mx-5 my-2" >
-            <h2 className="text-black">Name: <u>{name}</u></h2>
-            <h3>Current Balance: <span className={CashTotal<=minCash ? "text-red-500":"text-green-500"}>{CashTotal}</span></h3>
-            <h3>Balance Update: <span style={{color:"green"}}>{balanceAsOf} </span></h3>
-            <h3><span style={{color:"grey"}}>Last Update: {lastCommunication}</span></h3>
-            <h3><span style={{color:"grey"}}>ID: {TerminalID}</span></h3>
-            <h3><span style={{color:"grey"}}>Min Reload: {minCash}</span></h3>
-            <form id="minimal-reload" onSubmit={reloadUpdateHandler}>
-    <InfoForm reload={minReloadHandler} parentFormFunction={parentForm} minCash={minCash}/>
-    </form>
-            
-            </div>
-    
-          </div>
+  const withoutUpdateForm = (
+    <div
+      onClick={handleClickInside}
+      className="squares bg-white text-black my-4 max-w-xs rounded-lg border-gray-500 border-1 shadow-2xl m-0"
+    >
+      <div className="square-content flex flex-col mx-5 my-2">
+        <h2 className="text-black">
+          Name: <u>{name}</u>
+        </h2>
+        <h3>
+          Current Balance:{" "}
+          <span
+            className={CashTotal <= minCash ? "text-red-500" : "text-green-500"}
+          >
+            {CashTotal}
+          </span>
+        </h3>
+        <h3>
+          Balance Update: <span style={{ color: "green" }}>{balanceAsOf} </span>
+        </h3>
+        <h3>
+          <span style={{ color: "grey" }}>
+            Last Update: {lastCommunication}
+          </span>
+        </h3>
+        <h3>
+          <span style={{ color: "grey" }}>ID: {TerminalID}</span>
+        </h3>
+        <h3>
+          <span style={{ color: "grey" }}>Min Reload: {minCash}</span>
+        </h3>
+        <h4 className="text-center align-text-bottom">Click to Update</h4>
+      </div>
+    </div>
+  );
 
-const withoutUpdateForm = <div onClick={updateHandler} className="squares bg-white text-black my-4 max-w-xs rounded-lg border-gray-500 border-1 shadow-2xl m-0"  >
-            <div className="square-content flex flex-col mx-5 my-2" >
-            <h2 className="text-black">Name: <u>{name}</u></h2>
-            <h3>Current Balance: <span className={CashTotal<=minCash ? "text-red-500":"text-green-500"}>{CashTotal}</span></h3>
-            <h3>Balance Update: <span style={{color:"green"}}>{balanceAsOf} </span></h3>
-            <h3><span style={{color:"grey"}}>Last Update: {lastCommunication}</span></h3>
-            <h3><span style={{color:"grey"}}>ID: {TerminalID}</span></h3>
-            <h3><span style={{color:"grey"}}>Min Reload: {minCash}</span></h3>
-              <h4 className="text-center align-text-bottom" >Click to Update</h4>
-            </div>
-          </div>
+  // setInterval(refreshData, 10000);
 
-
-  setInterval(refreshData,10000)
-
-  return(
-
-    <>
- 
-    { update ? withUpdateForm:withoutUpdateForm }
-  
-    </>
-    
-
-  )
-          
-      };
-
+  return <>{update ? withUpdateForm : withoutUpdateForm}</>;
+};
 
 export default SquaresContent;
